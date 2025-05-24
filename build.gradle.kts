c7d7f4ceb5f934b0baf156d640f20ac143cc9c09
Shabalin13 <d.shabal1n12@gmail.com>
2025-05-25 02:15:25 +0300

    KP-10 feature: add data module

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.android.library) apply false
 }
 
 subprojects {
