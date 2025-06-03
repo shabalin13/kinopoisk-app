fbae557e0db178e49e176842bc52c6e8247141f9
Shabalin13 <d.shabal1n12@gmail.com>
2025-06-03 16:11:48 +0300

    KP-11 feature: add initial content component

---
+++ feature/media-catalog/src/main/java/com/shabalin13/kinopoisk/mediaCatalog/presentation/components/MediaCatalogInitialContent.kt
@@ -0,0 +1,44 @@
+package com.shabalin13.kinopoisk.mediaCatalog.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.shabalin13.kinopoisk.mediaCatalog.R
+import com.shabalin13.kinopoisk.ui.theme.KinopoiskTheme
+import com.shabalin13.kinopoisk.ui.theme.Paddings
+
+@Composable
+internal fun MediaCatalogInitialContent(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(R.string.initial_content_message),
+            style = MaterialTheme.typography.bodyLarge,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(Paddings.large)
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+internal fun MediaCatalogInitialContentPreview() {
+    KinopoiskTheme {
+        MediaCatalogInitialContent(
+            modifier = Modifier
+                .fillMaxSize()
+        )
+    }
+}
