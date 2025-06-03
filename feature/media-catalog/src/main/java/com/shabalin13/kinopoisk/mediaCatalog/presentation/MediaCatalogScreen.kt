fd1729590628104cebc13833c921c98d9c333b23
Shabalin13 <d.shabal1n12@gmail.com>
2025-06-03 16:50:35 +0300

    KP-11 feature: configure MediaCatalogScreen

--- feature/media-catalog/src/main/java/com/shabalin13/kinopoisk/mediaCatalog/presentation/MediaCatalogScreen.kt
+++ feature/media-catalog/src/main/java/com/shabalin13/kinopoisk/mediaCatalog/presentation/MediaCatalogScreen.kt
@@ -1,15 +1,87 @@
 package com.shabalin13.kinopoisk.mediaCatalog.presentation
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import com.shabalin13.kinopoisk.mediaCatalog.presentation.components.MediaCatalogDataContent
+import com.shabalin13.kinopoisk.mediaCatalog.presentation.components.MediaCatalogInitialContent
+import com.shabalin13.kinopoisk.mediaCatalog.presentation.components.MediaCatalogSearchBar
+import com.shabalin13.kinopoisk.ui.theme.Paddings
 
-@Suppress("UnusedParameter")
 @Composable
 internal fun MediaCatalogScreen(
     viewModel: MediaCatalogViewModel,
     onMediaCatalogItemClick: (mediaId: Int) -> Unit,
 ) {
-    Text(
-        "All is good"
-    )
+    val snackbarHostState = remember { SnackbarHostState() }
+    val focusManager = LocalFocusManager.current
+    Scaffold(
+        modifier = Modifier.pointerInput(Unit) {
+            detectTapGestures(onTap = {
+                focusManager.clearFocus()
+            })
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { innerPadding ->
+        val state by viewModel.state.collectAsState()
+
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .consumeWindowInsets(innerPadding)
+        ) {
+            var searchQuery by rememberSaveable { mutableStateOf("") }
+
+            LaunchedEffect(searchQuery) {
+                viewModel.handleIntent(
+                    MediaCatalogIntent.SearchMediaCatalogItemsForQuery(
+                        searchQuery
+                    )
+                )
+            }
+
+            MediaCatalogSearchBar(
+                searchQuery = searchQuery,
+                onQueryChange = { searchQuery = it },
+                onSearch = { focusManager.clearFocus() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(Paddings.small)
+            )
+
+            when (val currentState = state) {
+                MediaCatalogState.Initial -> MediaCatalogInitialContent(
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+
+                is MediaCatalogState.Data -> MediaCatalogDataContent(
+                    state = currentState,
+                    onMediaCatalogItemClick = onMediaCatalogItemClick,
+                    snackbarHostState = snackbarHostState,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            }
+        }
+    }
 }
