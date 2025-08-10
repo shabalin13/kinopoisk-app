53ed30d87427144f35cc7f71e5400f5842e469c0
Shabalin13 <d.shabal1n12@gmail.com>
2025-08-10 16:15:46 +0300

    KP-16 feature: add ActionButtonsInfoUiModel and confgure HeaderActionButtons component

--- feature/media-details/src/main/java/com/shabalin13/kinopoisk/mediaDetails/presentation/MediaDetailsIntent.kt
+++ feature/media-details/src/main/java/com/shabalin13/kinopoisk/mediaDetails/presentation/MediaDetailsIntent.kt
@@ -1,3 +1,7 @@
 package com.shabalin13.kinopoisk.mediaDetails.presentation
 
-internal sealed interface MediaDetailsIntent
+internal sealed interface MediaDetailsIntent {
+    data object RateButtonClicked : MediaDetailsIntent
+    data object ToggleWatchlistButtonClicked : MediaDetailsIntent
+    data object ShareButtonClicked : MediaDetailsIntent
+}
