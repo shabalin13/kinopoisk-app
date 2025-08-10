399026b865b00e6e5e25663b7d50722b2e8547f2
Shabalin13 <d.shabal1n12@gmail.com>
2025-08-10 15:47:12 +0300

    KP-16 feature: add MediaDetailsLoadingContent component

--- feature/media-details/src/main/java/com/shabalin13/kinopoisk/mediaDetails/presentation/MediaDetailsState.kt
+++ feature/media-details/src/main/java/com/shabalin13/kinopoisk/mediaDetails/presentation/MediaDetailsState.kt
@@ -1,3 +1,7 @@
 package com.shabalin13.kinopoisk.mediaDetails.presentation
 
 internal sealed interface MediaDetailsState
+internal sealed interface MediaDetailsState {
+    data object Initial : MediaDetailsState
+    data object Loading : MediaDetailsState
+}
---
+++ feature/media-details/src/main/java/com/shabalin13/kinopoisk/mediaDetails/presentation/components/MediaDetailsLoadingContent.kt
@@ -0,0 +1,32 @@
+package com.shabalin13.kinopoisk.mediaDetails.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shabalin13.kinopoisk.ui.theme.KinopoiskTheme
+
+@Composable
+internal fun MediaDetailsLoadingContent(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+internal fun MediaDetailsLoadingContentPreview() {
+    KinopoiskTheme {
+        MediaDetailsLoadingContent(
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
